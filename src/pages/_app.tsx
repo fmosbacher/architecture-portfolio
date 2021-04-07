bf48ae07f9f5c0c2e1c25f1e1b12802d1a9f5c0c
Fernando Azevedo <azevedo.fma@gmail.com>
2021-04-06 21:35:39 -0300

    Add favicon and og:title tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { AppProps } from 'next/app';
 import React from 'react';
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>Portfolio - Ariane Inocêncio</title>
+      <meta
+        property="og:title"
+        content="Portfolio - Ariane Inocêncio"
+        key="title"
+      />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
